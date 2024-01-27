357ac80b8c925e547423d508810006e5a559dc9d
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-27 15:55:32 +0300

    2.removed the equal sign in the get_hyper method

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -76,6 +76,6 @@ class Server:
                 'page': page,
                 'data': data,
                 'next_page': page + 1 if total_pages > page else None,
-                'prev_page': page - 1 if page >= 1 else None,
+                'prev_page': page - 1 if page > 1 else None,
                 'total_pages': total_pages
                 }
