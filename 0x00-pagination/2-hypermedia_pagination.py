2c97ef65d209f2624443836276d247c893f06b1e
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-27 15:51:12 +0300

    2.updated parameters passed in the dict (removed the equal signs in_page

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -69,16 +69,13 @@ class Server:
         """ creates and returns a dictionary
         of data given and page_size, page,
         data, next_page, prev_page, total_pages """
-        # import dataset
-        dataset = self.dataset()
-
         total_pages = math.ceil(len(self.dataset()) / page_size)
         data = self.get_page(page, page_size)
         return {
                 'page_size': len(data),
                 'page': page,
                 'data': data,
-                'next_page': page + 1 if total_pages >= page else None,
-                'prev_page': page - 1 if page >= 1 else None,
+                'next_page': page + 1 if total_pages > page else None,
+                'prev_page': page - 1 if page > 1 else None,
                 'total_pages': total_pages
                 }
