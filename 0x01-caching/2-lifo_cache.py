d0de7302db4e8d2dd9d1fdf7799d1674c1bb1b39
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-30 23:10:03 +0300

    2-lifo_cache.py

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+module that contains a cache
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    cache using FIFO algorithm
+    """
+    def __init__(self):
+        """
+        contains attr of this cache
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        adds an item in the cache
+        """
+        if key is None or item is None:
+            return
+
+        if self.cache_data.get(key, None):
+            del(self.cache_data[key])
+
+        self.cache_data[key] = item
+        max_items = BaseCaching.MAX_ITEMS
+        if len(self.cache_data.keys()) > max_items:
+            delete_item_key = list(self.cache_data.keys())[-2]
+            print(f"DISCARD: {delete_item_key}")
+            del(self.cache_data[delete_item_key])
+
+    def get(self, key):
+        """
+        gets an item using key
+        """
+        return self.cache_data[key] if self.cache_data.get(key, None) else None
