c8c48cbb96e6d17cc0c2fbc46104dd8dfe997c03
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-31 01:38:01 +0300

    5.LFU caching

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -5,16 +5,16 @@ module that contains a cache
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class MRUCache(BaseCaching):
+class LFUCache(BaseCaching):
     """
-    cache using MRU algorithm
+   cache using LFU algorithm
     """
     def __init__(self):
         """
         contains attr of this cache
         """
         super().__init__()
-        self.__mru = []
+        self.__mru = {}
 
     def put(self, key, item):
         """
@@ -24,32 +24,36 @@ class MRUCache(BaseCaching):
             return
 
         if self.cache_data.get(key, None):
+            self.__mru[key] += 1
             del(self.cache_data[key])
+        else:
+            self.__mru[key] = 1
 
-        self.__update_mru(key)
         self.cache_data[key] = item
         max_items = BaseCaching.MAX_ITEMS
         if len(self.cache_data.keys()) > max_items:
-            delete_item_key = self.__mru[-2]
+            delete_item_key = self.__lfu()
             print(f"DISCARD: {delete_item_key}")
             del(self.cache_data[delete_item_key])
-            del(self.__mru[self.__mru.index(delete_item_key)])
+            del(self.__mru[delete_item_key])
 
     def get(self, key):
         """
         gets an item using key
         """
         if self.cache_data.get(key, None):
-            self.__update_mru(key)
+            self.__mru[key] += 1
             return self.cache_data[key]
 
         return None
 
-    def __update_mru(self, key):
+    def __lfu(self):
         """
         least recently used algorithm
         updates mru list
         """
-        if key in self.__mru:
-            del(self.__mru[self.__mru.index(key)])
-        self.__mru.append(key)
+        minimum_value = min(list(self.__mru.values())[:-1])
+        print(self.__mru)
+        for key, value in self.__mru.items():
+            if value == minimum_value:
+                return key
