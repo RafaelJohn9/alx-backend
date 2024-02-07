56acdec63c50471388877805d14ac563e42cbbfc
RafaelJohn9 <rafaeljohb@gmail.com>
2024-02-07 23:13:15 +0300

    5.corrected some side parameters

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -58,7 +58,8 @@ def get_locale():
 
     user = getattr(g, 'user', None)
     if user is not None:
-        return user['locale']
+        if user['locale'] in app.config['LANGUAGES']:
+            return user['locale']
     return request.accept_languages.best_match(['en', 'fr'])
 
 
